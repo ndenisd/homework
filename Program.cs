33dfc0ef629d4eda93aa3a6991233f55c83c65f0
denis <ndenisd@yandex.ru>
2022-11-25 19:54:39 +0300

    add file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int max = 0;
+int min = 0;
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+    }
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+    }
+Console.WriteLine("max = " + max + " и " + "min = " + min);
